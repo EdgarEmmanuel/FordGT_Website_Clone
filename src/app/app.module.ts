d1a45f2b46a25c08e6153cfceed877619d6b4ac4
Edgar <mmnledgar@gmail.com>
2021-10-21 15:53:25 +0000

    make update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalNavComponent } from './modal-nav/modal-nav.component';
+import {SlideCarComponent} from "./slide-car/slide-car.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalNavComponent,
+    SlideCarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slide-car/slide-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-car',
+  templateUrl: './slide-car.component.html',
+  styleUrls: ['./slide-car.component.css']
+})
+export class SlideCarComponent {
+
+}
